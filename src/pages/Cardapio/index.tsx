79248884fc80a906b28d0d17d580a9646c940384
klaercio <klaerciosilva@gmail.com>
2022-05-26 20:03:09 -0300

    🔍Campo para busca adicionado

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,12 @@
 import style from './Cardapio.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg';
+import Buscador from './Buscador';
+import { useState } from 'react';
 
 export default function Cardapio() {
+
+    const [busca, setBusca] = useState("");
+
     return <>
         <main>
             <nav className={style.menu}>
@@ -11,6 +16,10 @@ export default function Cardapio() {
                 <div className={style.header__text}>
                     A casa do codigo e da massa
                 </div>
+                <section className={style.cardapio}>
+                    <h3 className={style.cardapio__titulo}>Cardápio</h3>
+                    <Buscador busca={busca} setBusca={setBusca} />
+                </section>
             </header>
         </main>
     </>
