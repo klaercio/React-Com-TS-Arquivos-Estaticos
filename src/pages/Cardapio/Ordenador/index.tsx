0acd43a217cd9dd192cba1d8bbc35555d234f067
klaercio <klaerciosilva@gmail.com>
2022-05-28 12:51:50 -0300

    🚀Mais opções de filtro adicionada

---
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -0,0 +1,40 @@
+import style from './Ordenador.module.scss';
+import opcoes from './opcoes.json';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import {MdKeyboardArrowUp, MdKeyboardArrowDown} from 'react-icons/md'
+
+interface OrdenadorProps {
+    ordenador : string,
+    setOrdenador : React.Dispatch<React.SetStateAction<string>>,
+}
+
+export default function Ordenador( {ordenador, setOrdenador} : OrdenadorProps) {
+    const [aberto, setAberto] = useState<boolean>(false);
+    const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
+
+    return (
+        <button 
+        className={classNames({
+            [style.ordenador] : true,
+            [style['ordenador--ativo']] : ordenador !== "",
+        })} 
+        onClick={()=> setAberto(!aberto)}
+        onBlur={()=> setAberto(false)}
+        >
+            <span>{nomeOrdenador || 'Ordenador Por'}</span>
+            {aberto ? <MdKeyboardArrowUp size={20}/>: <MdKeyboardArrowDown size={20}/>}
+            <div 
+            className={classNames({
+                [style.ordenador__options] : true,
+                [style['ordenador__options--ativo']] : aberto,
+            })}
+            >
+                {opcoes.map((opcao) => (
+                <div className={style.ordenador__option} key={opcao.value} onClick={()=> setOrdenador(opcao.value)}>
+                    {opcao.nome}
+                </div>))}
+            </div>
+        </button>
+    )
+}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,11 +3,13 @@ import {ReactComponent as Logo} from 'assets/logo.svg';
 import Buscador from './Buscador';
 import { useState } from 'react';
 import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 
 export default function Cardapio() {
 
     const [busca, setBusca] = useState("");
     const [filtro, setFiltro] = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("");
 
     return <>
         <main>
@@ -23,6 +25,7 @@ export default function Cardapio() {
                     <Buscador busca={busca} setBusca={setBusca} />
                     <div className={style.cardapio__filtros}>
                         <Filtros filtro={filtro} setFiltro={setFiltro}/>
+                        <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
                     </div>
                 </section>
             </header>
