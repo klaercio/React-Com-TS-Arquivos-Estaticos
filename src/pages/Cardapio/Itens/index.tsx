25f2c7d96f868411d097dec3e9216b6174e625f0
klaercio <klaerciosilva@gmail.com>
2022-05-29 13:40:41 -0300

    🔎Caixa de pesquisa, e filtro de categoria funcionando

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,11 +1,37 @@
 import cardapio from './itens.json';
 import Item from './Item';
 import style from './Itens.module.scss'
+import { useEffect, useState } from 'react';
 
-export default function Itens() {
+interface ItensProps {
+    busca : string,
+    filtro : number | null;
+    ordenador : string;
+}
+
+export default function Itens(props : ItensProps) {
+
+    const [lista, setLista] = useState(cardapio);
+    const { busca, filtro} = props;
+
+    function testaBusca(title : string) {
+        const regex = new RegExp(busca, 'i');
+        return regex.test(title);
+    }
+
+    function testaFiltro (id : number) {
+        if(filtro !== null) return filtro === id;
+        return true;
+    }
+
+    useEffect(()=> {
+       const novaLista = cardapio.filter( item => testaBusca(item.title) && testaFiltro(item.category.id));
+       setLista(novaLista);
+    }, [busca, filtro])
+
     return (
         <div className={style.itens}>
-            {cardapio.map(item => (
+            {lista.map(item => (
                 <Item key={item.id}
                 {...item}/>
             ))}
