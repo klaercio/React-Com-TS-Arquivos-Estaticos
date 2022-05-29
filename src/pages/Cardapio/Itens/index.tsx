b167b332eb8cd8d98849f47665150aaec34aaa1a
klaercio <klaerciosilva@gmail.com>
2022-05-29 13:49:46 -0300

    🚀Ordenador funcionando e projeto finalizado

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -12,7 +12,7 @@ interface ItensProps {
 export default function Itens(props : ItensProps) {
 
     const [lista, setLista] = useState(cardapio);
-    const { busca, filtro} = props;
+    const { busca, filtro, ordenador} = props;
 
     function testaBusca(title : string) {
         const regex = new RegExp(busca, 'i');
@@ -24,10 +24,23 @@ export default function Itens(props : ItensProps) {
         return true;
     }
 
+    function ordenar(novaLista : typeof cardapio) {
+        switch(ordenador) {
+            case 'porcao' :
+                return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+            case 'qtd-pessoas' : 
+                return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
+            case 'preco' : 
+                return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
+            default:
+                return novaLista;
+            }
+    }
+
     useEffect(()=> {
        const novaLista = cardapio.filter( item => testaBusca(item.title) && testaFiltro(item.category.id));
-       setLista(novaLista);
-    }, [busca, filtro])
+       setLista(ordenar(novaLista));
+    }, [busca, filtro, ordenador])
 
     return (
         <div className={style.itens}>
