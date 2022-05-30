6c695067791f6bf53473a49cab543bb0cbf8080e
klaercio <klaerciosilva@gmail.com>
2022-05-30 20:21:32 -0300

    🚀Rotas inicio e cardapio definidas

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,29 @@
+import cardapio from 'data/itens.json';
+import style from './Inicio.module.scss';
+
+export default function Inicio() {
+
+    let pratosRecomendados = [...cardapio];
+
+    pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0,3);
+
+    return(
+        <section>
+            <h3 className={style.titulo}>
+                Recomendações da cozinha
+            </h3>
+            <div className={style.recomendados}>
+                {pratosRecomendados.map( item => (
+                    <div key={item.id} className={style.recomendado}>
+                        <div className={style.recomendado__imagem}>
+                            <img src={item.photo} alt={item.title} />
+                        </div>
+                        <button className={style.recomendado__botao}>
+                            Ver mais
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
