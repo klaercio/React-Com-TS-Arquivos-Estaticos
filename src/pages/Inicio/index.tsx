9864d119f9985ca4946d4cff73080ba91af99aba
klaercio <klaerciosilva@gmail.com>
2022-06-01 20:34:41 -0300

    📚Pagina sobre finalizada

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,6 +1,7 @@
 import cardapio from 'data/itens.json';
 import style from './Inicio.module.scss';
 import styleTema from 'styles/Tema.module.scss'; 
+import nossaCasa from 'assets/nossa_casa.png';
 
 export default function Inicio() {
 
@@ -24,6 +25,13 @@ export default function Inicio() {
                         </button>
                     </div>
                 ))}
+            </div>
+            <h3 className={styleTema.titulo}> Nossa casa</h3>
+            <div className={style.nossaCasa}>
+                <img src={nossaCasa} alt="Casa do aluroni" />
+                <div className={style.nossaCasa__endereco}>
+                    Rua vergueiro, 3185<br/><br/> Vila Mariana SP.
+                </div>
             </div>
         </section>
     );
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Header from 'components/Header';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import Sobre from 'pages/Sobre/Index';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Menu from './components/Menu';
 
@@ -13,6 +14,7 @@ export default function AppRouter() {
                     <Route path='/' element={<Header/>}>
                         <Route index element={<Inicio/>}/>
                         <Route path='cardapio' element={<Cardapio/>}/>
+                        <Route path='sobre' element={<Sobre/>}></Route>
                     </Route>
                 </Routes>
             </Router>
